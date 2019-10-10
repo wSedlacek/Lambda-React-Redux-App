f1f6dc7fb524c708a6e804f005f90ca48bcfff96
William Sedlacek <wsedlacekc@gmail.com>
2019-10-10 13:10:50 -0500

    Style Card
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/app/components/cards-list/cards-list.component.tsx
+++ src/app/components/cards-list/cards-list.component.tsx
@@ -4,16 +4,26 @@ import { useSelector } from 'react-redux';
 import { MagicCard } from '../../models/MagicCard';
 import { State } from '../../state/app.reducer';
 import { MagicCardComponent } from '../magic-card/magic-card.component';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    height: '100vh',
+    display: 'grid',
+    justifyContent: 'center',
+  },
+});
 
 const CardsListComponent = () => {
   const cards = useSelector<State, MagicCard[]>((state) => state.cards.list);
+  const classes = useStyles({});
 
   return (
-    <>
+    <div className={classes.root}>
       {cards.map((card) => (
-        <MagicCardComponent card={card} />
+        <MagicCardComponent key={card.id} card={card} />
       ))}
-    </>
+    </div>
   );
 };
 
--- src/app/components/magic-card/magic-card.component.tsx
+++ src/app/components/magic-card/magic-card.component.tsx
@@ -1,7 +1,48 @@
 import React from 'react';
 
-import { Card, Typography } from '@material-ui/core';
+import { Card, CardContent, Typography, makeStyles, CardMedia } from '@material-ui/core';
 import { MagicCard } from '../../models/MagicCard';
+
+interface StyleProps {
+  color: string;
+}
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: (props: StyleProps) => {
+      switch (props.color) {
+        case 'R':
+          return 'red';
+        case 'G':
+          return 'green';
+        case 'B':
+          return 'black';
+        case 'W':
+          return 'yellow';
+        case 'U':
+          return 'blue';
+        default:
+          return 'white';
+      }
+    },
+    color: (props: StyleProps) => {
+      switch (props.color) {
+        case 'W':
+          return 'black';
+        case undefined:
+          return 'black';
+        default:
+          return 'white';
+      }
+    },
+    height: 500,
+    width: 600,
+  },
+  img: {
+    height: 400,
+    width: 600,
+  },
+});
 
 type MagicCardProps = {
   card: MagicCard;
@@ -9,11 +50,16 @@ type MagicCardProps = {
 
 const MagicCardComponent = (props: MagicCardProps) => {
   const { card } = props;
+  const classes = useStyles({ color: card.color_identity[0] });
+
   return (
-    <Card>
-      <Typography variant='h3' gutterBottom>
-        {card.name}
-      </Typography>
+    <Card className={classes.root}>
+      <CardMedia className={classes.img} component='img' image={card.image_uris.art_crop} />
+      <CardContent>
+        <Typography variant='h3' gutterBottom>
+          {card.name}
+        </Typography>
+      </CardContent>
     </Card>
   );
 };
